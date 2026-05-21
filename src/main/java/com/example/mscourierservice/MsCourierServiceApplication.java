8465993c8c437e15545844733772a037fde7e40f
xeyal-java <xeyalqasimov93@gmail.com>
2026-05-21 15:21:50 +0400

    first commit

---
+++ src/main/java/com/example/mscourierservice/MsCourierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscourierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCourierServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCourierServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mscourierservice/MsCourierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscourierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCourierServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
